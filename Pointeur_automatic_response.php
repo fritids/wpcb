248253a52b629a75fd6f53db063373e4e493d199
6WWW <6WWW@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-26 10:27:20 +0000

    3.3 compatibility
    git-svn-id: https://plugins.svn.wordpress.org/wpcb/tags/1.1.3@480677 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Pointeur_automatic_response.php
+++ Pointeur_automatic_response.php
@@ -1,4 +1,4 @@
 <?php
-// This file is beeing copied to root of your website when activating the atos plugin.
+// This file is beeing copied to root of your website when activating the atos plugin, don't modify
 include 'wp-content/plugins/wpcb/automatic_response.php';
 ?>
