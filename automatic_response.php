9ca99d7067e40d07db320e6ae4b9d3a73ed79631
6WWW <6WWW@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-27 20:58:42 +0000

    2.4.2
    git-svn-id: https://plugins.svn.wordpress.org/wpcb/tags/2.4.2@591112 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- automatic_response.php
+++ automatic_response.php
@@ -8,10 +8,10 @@ if (!$wpcb_general_options){
 	echo 'Vous n\'avez pas renseigné les options atos dans les réglages du plugin wpcb<br/>';
 }
 if ($_GET['debug']==1){
-echo '$wpcb_general_options -> <br/>';
-print_r($wpcb_general_options);
-echo '<br/>$wpcb_dev_options -> <br/>';
-print_r($wpcb_dev_options);
+	echo '$wpcb_general_options -> <br/>';
+	print_r($wpcb_general_options);
+	echo '<br/>$wpcb_dev_options -> <br/>';
+	print_r($wpcb_dev_options);
 }
 $purch_log_email=get_option('purch_log_email');
 if (!$purch_log_email){$purch_log_email=get_bloginfo('admin_email');}
@@ -25,8 +25,10 @@ else{
 	$path_bin_response=$wpcb_cb_options['path_bin_response'];
 	$logfile=$wpcb_cb_options['logfile'];
 }
+
 if ($wpcb_dev_options['mode_debugatos']){
-	wp_mail($purch_log_email,'Email envoyé depuis le auto_response','Found');
+	$log.='$pathfile'.$pathfile."\n";
+	$log.='$path_bin_response'.$path_bin_response."\n";
 }
 // Initialisation du chemin du fichier de log :
 if (isset($_POST['DATA'])){
@@ -78,123 +80,82 @@ if (isset($_POST['DATA'])){
 	//Session id used by wp ecommerce :
 	$sessionid=$response['orderid'];
 
-	// A venir : Ajout dans un google spreadsheet qui a toutes les entêtes précédentes (requis Zend)
-	// A coler dans la page admin pour tester
-
-
-		if (WP_ZEND_FRAMEWORK){
-			$GoogleConnection=true;
-			$SpreadSheetConnection=true;
-			try {$client = Zend_Gdata_ClientLogin::getHttpClient($wpcb_general_options['googleemail'],$wpcb_general_options['googlepassword']);}
-			catch (Zend_Gdata_App_AuthException $ae){echo $ae->exception();$GoogleConnection=false;}
-			if ($GoogleConnection){			
-				// Test 
-				$service=Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME;
-				$client=Zend_Gdata_ClientLogin::getHttpClient($wpcb_general_options['googleemail'],$wpcb_general_options['googlepassword'], $service);
-				// On va chercher le numéro de la feuille :
-				$query_worksheet = new Zend_Gdata_Spreadsheets_DocumentQuery(); // todo pour pas de client ici ?
-				$query_worksheet->setSpreadsheetKey($wpcb_general_options['spreadsheetKey']);
-				$spreadsheetService = new Zend_Gdata_Spreadsheets($client);
-				try {$feed = $spreadsheetService->getWorksheetFeed($query_worksheet);}
-				catch (Zend_Gdata_App_HttpException $ae){echo $ae->exception();$SpreadSheetConnection=false;}
-				if ($SpreadSheetConnection){
-					// Tout bon on ajoute : 
-					foreach($feed->entries as $entry){
-						if ($entry->title->text=='CB'){$worksheetId_CB=basename($entry->id);}
-						elseif ($entry->title->text=='Paypal'){$worksheetId_Paypal=basename($entry->id);}
-					}
-					$spreadsheetService = new Zend_Gdata_Spreadsheets($client);
-					// Insert row in google spreadsheet :
-					$insertedListEntry = $spreadsheetService->insertRow($response,$wpcb_general_options['spreadsheetKey'],$worksheetId_CB);
-				}
-				else{
-					wp_mail($purch_log_email,'Email envoyé depuis le auto_response','Mauvais Numero de Spreadsheet dans les options du plugin wpcb');
-				}
-			}
-			else {
-				if ($wpcb_dev_options['mode_debugatos']){
-				wp_mail($purch_log_email,'Email envoyé depuis le auto_response','Mauvais login/mot de pass google dans les options du plugin wpcb');
-				}
-			}
-		}
-		else{
-			wp_mail($purch_log_email,'Email envoyé depuis le auto_response','Installer Zend pour ajouter automatiquement les ventes à google drive !');
-		}
-	
+if ($wpcb_dev_options['mode_debugatos']){
+	$log.="\n\n"."-----------SALES (You receive this email because mode debug atos is checked----------------------------\n";
+	foreach ($response as $k => $v) {
+		$log.= $k." = ".$v."\n";
+	}
+}
 
 	//  analyse du code retour
 	if ( isset($_POST['sandbox']) ){
-			$message="-----------SANDBOX-------------------------\n";
+			$log="-----------SANDBOX-------------------------\n";
 			foreach ($response as $k => $v) {
-				$message.= $k." = ".$v."\n";
+				$log.= $k." = ".$v."\n";
 			}
-			$message.="-------------------------------------------\n";
+			$log.="-------------------------------------------\n";
 			if ($logfile){	
 				$fp=fopen($logfile, "a");
-				fwrite($fp,$message);
+				fwrite($fp,$log);
 				fclose ($fp);
 			}
-			echo $message;
-			wp_mail($purch_log_email,'Email pour vous dire qu\'un paiement SANDBOX est arrivé !',$message);
+			echo $log;
+			wp_mail($purch_log_email,'Email pour vous dire qu\'un paiement SANDBOX est arrivé !',$log);
 	} // Fin de l'achat sandbox
 	else{ //Vrai achat !
-	if (($response['code']=="") && ($response['error']=="")){
-		$message="erreur appel response\n executable response non trouve $path_bin_response\n Session Id : $sessionid";
-		if ($logfile){
-			$fp=fopen($logfile,"a");			// Ouverture du fichier de log en append
-			fwrite($fp,$message);
-			fclose ($fp);
-		}
-		if (WP_DEBUG){
-			wp_mail($purch_log_email,'Email envoyé depuis le auto_response car il y a une erreur avec un paiement Atos',$message);
-		}
-		$wpdb->query("UPDATE `".WPSC_TABLE_PURCHASE_LOGS."` SET `processed`= '5' WHERE `sessionid`=".$sessionid);
-		$wpsc_cart->empty_cart();
-	}
-	elseif ($response['code']!=0){
-		$message=" API call error.\n Error message :  $error\n Session Id : $sessionid";	
-		if ($logfile){
-			// Ouverture du fichier de log en append
-			$fp=fopen($logfile, "a");
-			fwrite($fp,$message);
-			fclose ($fp); 
-		}
-		if ($wpcb_dev_options['mode_debugatos']){
-			wp_mail($purch_log_email,'Email envoyé depuis le auto_response car il y a une erreur avec un paiement Atos',$message);
-		}
-		$wpdb->query("UPDATE `".WPSC_TABLE_PURCHASE_LOGS."` SET `processed`= '5' WHERE `sessionid`=".$sessionid);
-		$wpsc_cart->empty_cart();
-	 }
-	else{
-		// Ok, Sauvegarde dans la base de donnée du shop.
-		if ($response['code']==00) {
-			$message="-----------SALES----------------------------\n";
-			foreach ($response as $k => $v) {
-				$message.= $k." = ".$v."\n";
+		if (($response['code']=="") && ($response['error']=="")){
+			$log.="-----------ERROR----------------------------\n";
+			$log.="erreur appel response\n executable response non trouve ".$path_bin_response."\n". "Session Id : ".$sessionid."\n";
+			$log.="-------------------------------------------\n";
+			if ($logfile){
+				$fp=fopen($logfile,"a");			// Ouverture du fichier de log en append
+				fwrite($fp,$log);
+				fclose ($fp);
 			}
-			$message.="-------------------------------------------\n";
-			if ($logfile){	
-				$fp=fopen($logfile, "a");
-				fwrite($fp,$message);
-				fclose($fp);
-			}
-			if ($wpcb_dev_options['mode_debugatos']){
-				wp_mail($purch_log_email,'Email pour vous dire qu\'un paiement est arrivé !',$message);
-			}
-			$wpdb->query("UPDATE `".WPSC_TABLE_PURCHASE_LOGS."` SET `processed`= '3' WHERE `sessionid`=".$sessionid);
-			$purchase_log = $wpdb->get_row("SELECT * FROM `".WPSC_TABLE_PURCHASE_LOGS."` WHERE `sessionid`= ".$sessionid." LIMIT 1",ARRAY_A) ; // Ne pas enlever car global !
+			$wpdb->query("UPDATE `".WPSC_TABLE_PURCHASE_LOGS."` SET `processed`= '5' WHERE `sessionid`=".$sessionid);
 			$wpsc_cart->empty_cart();
-			// Peut-être faut-il ici decrease stock ???
-			// redirect ->
-			transaction_results($sessionid,false);
-			// false -> no echo ! // The cart is emptied in this function a condition d'avoir la global $wpsc_cart !
 		}
-	}
-}// Fin du vrai achat
-	}// Fin du isset POST DATA
+		elseif ($response['code']!=0){
+			$log.="-----------ERROR----------------------------\n";
+			$log.=" API call error.\n Error log :  $error\n Session Id : $sessionid";	
+			$log.="-------------------------------------------\n";
+			if ($logfile){
+				// Ouverture du fichier de log en append
+				$fp=fopen($logfile, "a");
+				fwrite($fp,$log);
+				fclose ($fp); 
+			}
+			$wpdb->query("UPDATE `".WPSC_TABLE_PURCHASE_LOGS."` SET `processed`= '5' WHERE `sessionid`=".$sessionid);
+			$wpsc_cart->empty_cart();
+		 }
+		else{
+			// Ok, Sauvegarde dans la base de donnée du shop.
+			if ($response['code']==00) {
+				$log.="-----------SALES----------------------------\n";
+				foreach ($response as $k => $v) {
+					$log.= $k." = ".$v."\n";
+				}
+				$log.="-------------------------------------------\n";
+				if ($logfile){	
+					$fp=fopen($logfile, "a");
+					fwrite($fp,$log);
+					fclose($fp);
+				}
+				$wpdb->query("UPDATE `".WPSC_TABLE_PURCHASE_LOGS."` SET `processed`= '3' WHERE `sessionid`=".$sessionid);
+				$purchase_log = $wpdb->get_row("SELECT * FROM `".WPSC_TABLE_PURCHASE_LOGS."` WHERE `sessionid`= ".$sessionid." LIMIT 1",ARRAY_A) ; // Ne pas enlever car global !
+				$wpsc_cart->empty_cart();
+				// Peut-être faut-il ici decrease stock ???
+				// redirect ->
+				transaction_results($sessionid,false);
+				// false -> no echo ! // The cart is emptied in this function a condition d'avoir la global $wpsc_cart !
+			}// Fin du  if response_code==0
+		}// fin des test d'errer
+	}// Fin du vrai achat
+}// Fin du isset POST DATA
 else{
-	if ($wpcb_dev_options['mode_debugatos']){
-			wp_mail($purch_log_email,'Qqn a accéder à cette page sans utiliser le module de CB','Rien de grave, c\'est peut-etre un robot google !');
-		}
+	// si pas post data do nothing !
 }
+
+
+//wp_mail($purch_log_email,'Automatic Response was called',$log);
 ?>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 Plugin Name: WPCB
 Plugin URI: http://wpcb.fr
 Description: Plugin de paiement par CB, paypal, ... et de calcul de frais de port (WP e-Commerce requis)
-Version: 2.4
+Version: 2.4.2
 Author: 6WWW
 Author URI: http://6www.net
 */
@@ -50,6 +50,11 @@ function wpcb_update(){
   }
   // nothing found, you may advice the user to install the ZF plugin
   define('WP_ZEND_FRAMEWORK', false);
+  	$merchantfiles=array('atos','cheque','virement','simplepaypal','systempaycyberplus');
+	foreach ($merchantfiles as $merchantfile){
+		copy(dirname(__FILE__).'/'.$merchantfile.'.merchant.php',dirname(dirname(__FILE__)).'/wp-e-commerce/wpsc-merchants/'.$merchantfile.'.merchant.php');
+	}
+	copy(dirname(__FILE__).'/automatic_response.php',dirname(dirname(dirname(dirname(__FILE__)))).'/automatic_response.php');
 }
 
 // Lors de la desinstallation : 
@@ -348,7 +353,9 @@ function wpcb_intialize_atos_options() {
 	add_settings_field('wpec_atos_display_name','wpec_atos_display_name','wpcb_wpec_atos_display_name_callback','wpcb_atos','atos_settings_section');
 	add_settings_field('wpec_atos_gateway_image','wpec_atos_gateway_image','wpcb_wpec_atos_gateway_image_callback','wpcb_atos','atos_settings_section');
 	add_settings_field('logfile','logfile','wpcb_logfile_callback','wpcb_atos','atos_settings_section');
-	
+	add_settings_field('title_page_logo_cb','Titre de la page des logos CB','wpcb_title_page_logo_cb_callback','wpcb_atos','atos_settings_section');	
+	add_settings_field('display_before_creditcardlogo','Texte de la page avant les logos CB','wpcb_display_before_creditcardlogo_callback','wpcb_atos','atos_settings_section');	
+
 
 	register_setting('wpcb_atos','wpcb_atos','');
 } // end wpcb_intialize_atos_options  
@@ -487,6 +494,22 @@ function wpcb_logfile_callback(){
     echo '<input type="text" '.$style.' size="75"id="logfile" name="wpcb_atos[logfile]" value="' . $val . '" />';  
 }
 
+function wpcb_title_page_logo_cb_callback(){  
+    $options = get_option( 'wpcb_atos');  
+    $val = 'Paiment sécurisé'; 
+    if(isset($options['title_page_logo_cb'])){$val = $options['title_page_logo_cb'];}
+    echo '<input type="text" '.$style.' size="75" id="title_page_logo_cb" name="wpcb_atos[title_page_logo_cb]" value="' . $val . '" />';  
+}
+
+
+
+function wpcb_display_before_creditcardlogo_callback() {  
+    $options = get_option( 'wpcb_atos');  
+    $val = "Cliquez sur la carte de votre choix ci dessous, vous allez être redirigé vers le site sécurisé Atos."; 
+    if(isset($options['display_before_creditcardlogo'])){$val = $options['display_before_creditcardlogo'];}
+    echo '<textarea type="textarea" id="display_before_creditcardlogo" name="wpcb_atos[display_before_creditcardlogo]" rows="7" cols="50">'.$val .'</textarea>';  
+}
+
 /** 
 * Cheque options
 */  
@@ -502,6 +525,7 @@ add_action('admin_init', 'wpcb_intialize_cheque_options');
 function wpcb_cheque_callback() {  
     echo '<p>Réglage des options pour le paiement par chèque</p>';  
 }
+
 
 function wpcb_displaycheque_callback() {  
     $options = get_option( 'wpcb_cheque');  
@@ -1858,10 +1882,10 @@ function AddSaleToGoogleSpreadsheet($purchase_log_id){
 
 
 function wpcb_display_payment_icon_page_content($content) {
-	$message.=$content;
 	global $wpdb, $purchase_log, $wpsc_cart;
-  if ($_GET['action'] == 'securepayment') {
-
+  	if ($_GET['action'] == 'securepayment') {
+	$wpcb_atos = get_option ( 'wpcb_atos' );
+	$message.=$wpcb_atos['display_before_creditcardlogo'].'<br/>';
 	$sessionid=$_GET['sessionid'];
 	$wpcb_general = get_option( 'wpcb_general' );
 	$wpcb_dev=get_option( 'wpcb_dev' );
@@ -1869,7 +1893,6 @@ function wpcb_display_payment_icon_page_content($content) {
 	$purch_log_email=get_option('purch_log_email');
 	if (!$purch_log_email){$purch_log_email=get_bloginfo('admin_email');}
 	if ($_GET['gateway']=='atos'){
-		$wpcb_atos = get_option ( 'wpcb_atos' );
 		// cf. Dictionnaire des Données Atos :
 		if (($wpcb_dev['mode_demo']) && (array_key_exists('mode_demo', $wpcb_dev)) ){
 			$merchant_id="082584341411111";
@@ -1904,7 +1927,7 @@ function wpcb_display_payment_icon_page_content($content) {
 			//Va afficher sur la page ou se trouve le shortcode les parametres.
 			$parm_pretty=str_replace(' ','<br/>',$parm);
 			echo '<p>You see this because you are in debug mode :</p><pre>'.$parm_pretty.'<br/>path_bin_request='.$path_bin_request.'</pre>';
-			echo'<p>End of debug mode</p>';
+			echo '<p>End of debug mode</p>';
 		}
 		$result=exec("$path_bin_request $parm");
 		$tableau=explode ("!","$result");
@@ -1920,7 +1943,7 @@ function wpcb_display_payment_icon_page_content($content) {
 		}
 		else{
 			// Affiche le formulaire avec le choix des cartes bancaires :
-			$message = $tableau[3];
+			$message.= $tableau[3];
 		}
 		// End of atos
 	}
@@ -1928,10 +1951,10 @@ function wpcb_display_payment_icon_page_content($content) {
 	$wpcb_paypal = get_option ( 'wpcb_paypal' );
 	$purchase_log=$wpdb->get_row("SELECT * FROM `".WPSC_TABLE_PURCHASE_LOGS."` WHERE `sessionid`= ".$sessionid." LIMIT 1") ;
 		if ($wpcb_paypal['sandbox_paypal']){
-			$message='<form action="https://sandbox.paypal.com/cgi-bin/webscr" method="post">';
+			$message.='<form action="https://sandbox.paypal.com/cgi-bin/webscr" method="post">';
 		}
 		else{
-			$message='<form action="https://www.paypal.com/cgi-bin/webscr" method="post">';
+			$message.='<form action="https://www.paypal.com/cgi-bin/webscr" method="post">';
 		}
 		$message.='<input type="hidden" name="cmd" value="_xclick">';
 		$message.='<input type="hidden" name="business" value="'.$wpcb_paypal['business'].'">';
@@ -2000,6 +2023,9 @@ function wpcb_display_payment_icon_page_content($content) {
 		$wpsc_cart->empty_cart();
 	}
   }
+  else {
+  $message=$content;	
+  }
 	return $message;
 }
 add_filter( 'the_content', 'wpcb_display_payment_icon_page_content' );
@@ -2008,7 +2034,8 @@ function wpcb_display_payment_icon_page_title($title, $id) {
     global $id;
     $intheloop=in_the_loop(); 
 	if ($intheloop && $id && ($_GET['action'] == 'securepayment')){
-	$title='Paiement Sécurisé';
+		$options=get_option('wpcb_atos');
+		$title=$options['title_page_logo_cb'];
     }
 else {
 	// Nothing
